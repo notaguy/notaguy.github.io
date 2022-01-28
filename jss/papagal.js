f7f225c6c725ed5b6edabee58f628ed3cfdb68ee
notaguy <95541662+notaguy@users.noreply.github.com>
2022-01-28 23:05:02 +0200

    28.01.2022-seara

--- jss/papagal.js
+++ jss/papagal.js
@@ -6,3 +6,13 @@ document.getElementById('butonas').addEventListener('click',function(){
 document.querySelector('.close').addEventListener('click',function(){
     document.querySelector('.bg-modal').style.display="none";
 });
+
+
+document.getElementById('butonas2').addEventListener('click',function(){
+  document.querySelector('.bg-modal2').style.display='flex';
+
+});
+
+document.querySelector('.close2').addEventListener('click',function(){
+  document.querySelector('.bg-modal2').style.display="none";
+});
