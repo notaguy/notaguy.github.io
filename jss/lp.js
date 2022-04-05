7ec1d6bd0308da819bab2fc03f35b6dcf142b649
notaguy <95541662+notaguy@users.noreply.github.com>
2022-04-05 12:01:55 +0300

    5/04/2022
    am mai schimbat ceva

--- jss/lp.js
+++ jss/lp.js
@@ -6,7 +6,7 @@ function vanish()
 }
 document.addEventListener('DOMContentLoaded', vanish);
 
-
+/*
 (function () {
     const second = 1000,
           minute = second * 60,
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', vanish);
           }
           //seconds
         }, 0)
-    }());
+    }());*/
  
     $(document).ready(function(){
       $('.chenar').slick({
